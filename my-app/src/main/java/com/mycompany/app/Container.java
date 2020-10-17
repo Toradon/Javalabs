9027d06318fb161af6ccef1839aa6a936eb05c41
Toradon <68597147+Toradon@users.noreply.github.com>
2020-10-17 18:57:07 +0300

    1labcomplete

---
+++ my-app/src/main/java/com/mycompany/app/Container.java
@@ -0,0 +1,86 @@
+package com.mycompany.app;
+
+/**
+ * Класс для хранения величин типа int
+ *
+ */
+public class Container
+{
+	private int[] array = null;
+
+	public void resize(int newSize)
+	{
+		if (newSize == 0) array = null;
+
+		int[] newArray = new int[newSize];
+
+		int minSize = newSize;
+		if (minSize > getSize()) minSize = getSize();
+
+		for (int i = 0; i < minSize; i++)
+		{
+			newArray[i] = array[i];
+		}
+		array = newArray;
+	}
+
+	/** Возвращает количество элементов в контейнере
+	 * @return Целочисленное значение количества элементов.     
+	*/
+
+	public int getSize()
+	{
+		if (array == null) return 0;
+		else return array.length;
+	}
+
+	/** Устанавливает значение элемента
+	 * @param index Индекс элемента 
+	 * @param value Значение элемента     
+	*/
+
+	public void set(int index, int value) throws ContainerException
+	{
+		if (index >= getSize() || index < 0 || array == null)
+			throw new ContainerException();
+		array[index] = value;
+	}
+
+	/** Возвращает значение элемента
+	 * @param index Индекс элемента 
+	 * @return Целочисленное значение элемента.  
+	*/
+
+	public int get(int index) throws ContainerException
+	{
+		if (index >= getSize() || index < 0 || array == null)
+			throw new ContainerException();
+		return array[index];
+	}
+
+	/** Добавляет элемент в конец контейнера
+	 * @param value Значение элемента     
+	*/
+
+	public void add(int value) throws ContainerException
+	{
+		resize(getSize() + 1);
+		set(getSize()-1, value);
+	}
+
+	/** Убирает элемент с заданным индексом из контейнера
+	 * @param index Индекс элемента     
+	*/
+
+	public void remove(int index) throws ContainerException
+	{
+		if (index >= getSize() || index < 0 || array == null)
+			throw new ContainerException();
+
+		for (int i = index; i < getSize() - 1; i++)
+		{
+			set(i, get(i+1));
+		}
+		resize(getSize()-1);
+	}
+}
--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -10,11 +10,59 @@ import org.junit.Test;
 public class AppTest 
 {
     /**
-     * Rigorous Test :-)
+     * Проверяем создание контейнера
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void newTest()
     {
-        assertTrue( true );
+    	Container c = new Container();
+        assertTrue( c.getSize() == 0 );
+    }
+
+	/**
+     * Проверяем изменение размера
+     */
+	@Test
+    public void resizeTest()
+    {
+    	Container c = new Container();
+    	c.resize(3);
+        assertTrue( c.getSize() == 3 );
+    }
+
+    /**
+     * Проверяем добавление элемента
+     */
+    @Test
+    public void addTest()
+    {
+		try
+		{
+			Container c = new Container();
+			c.add(3);
+			assertTrue( c.getSize() == 1 );
+			assertTrue( c.get(0) == 3 );
+		} catch (ContainerException e) {
+			 assertTrue(false);
+		}
+    }
+    /**
+     * Проверяем удаление элемента
+     */
+    @Test
+    public void removeTest()
+    {
+		try
+		{
+			Container c = new Container();
+			c.add(1);
+			c.add(2);
+			c.add(3);
+			c.remove(1);
+			assertTrue( c.getSize() == 2 );
+			assertTrue( c.get(1) == 3 );
+		} catch (ContainerException e) {
+			 assertTrue(false);
+		}
     }
 }
